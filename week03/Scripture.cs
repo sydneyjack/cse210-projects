18979dde53c67b92c1763cf91744820eff28f049
Sobomabo Boma Sydney <davidsydney.sb@gmail.com>
2026-05-30 10:37:47 +0100

    commit and push

---
+++ week03/Scripture.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] words = text.Split(" ");
+
+        foreach (string word in words)
+        {
+            Word newWord = new Word(word);
+            _words.Add(newWord);
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide)
+        {
+            int index = random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+
+            if (IsCompletelyHidden())
+            {
+                break;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = "";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return $"{_reference.GetDisplayText()} - {text}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,85 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video(
+            "Learn C# in 20 Minutes",
+            "Programming Hub",
+            1200);
+
+        video1.AddComment(
+            new Comment("Sydney", "Very helpful tutorial."));
+        video1.AddComment(
+            new Comment("John", "I finally understand classes."));
+        video1.AddComment(
+            new Comment("Sarah", "Great explanation."));
+
+        videos.Add(video1);
+
+        Video video2 = new Video(
+            "How to Build a Website",
+            "Tech Academy",
+            950);
+
+        video2.AddComment(
+            new Comment("Michael", "Amazing content."));
+        video2.AddComment(
+            new Comment("Grace", "Easy to follow."));
+        video2.AddComment(
+            new Comment("Daniel", "Thanks for sharing."));
+
+        videos.Add(video2);
+
+        Video video3 = new Video(
+            "Graphic Design Tips",
+            "Creative Studio",
+            700);
+
+        video3.AddComment(
+            new Comment("Emily", "Very useful."));
+        video3.AddComment(
+            new Comment("David", "Loved the examples."));
+        video3.AddComment(
+            new Comment("James", "Helped me improve my designs."));
+
+        videos.Add(video3);
+
+        Video video4 = new Video(
+            "Social Media Marketing",
+            "Marketing Pro",
+            800);
+
+        video4.AddComment(
+            new Comment("Sophia", "Excellent advice."));
+        video4.AddComment(
+            new Comment("Chris", "I learned a lot."));
+        video4.AddComment(
+            new Comment("Olivia", "Great strategies."));
+
+        videos.Add(video4);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Title: {video._title}");
+            Console.WriteLine($"Author: {video._author}");
+            Console.WriteLine($"Length: {video._length} seconds");
+            Console.WriteLine($"Comments: {video.GetNumberOfComments()}");
+
+            Console.WriteLine("\nComment List:");
+
+            foreach (Comment comment in video._comments)
+            {
+                Console.WriteLine(
+                    $"{comment._commenterName}: {comment._commentText}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }

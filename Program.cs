33a8f0c24a88750b29925cd45df34e0ca248b92e
Sobomabo Boma Sydney <davidsydney.sb@gmail.com>
2026-06-14 14:04:34 +0100

    push and commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(
+            new Running(
+                "03 Nov 2022",
+                30,
+                4.8));
+
+        activities.Add(
+            new Cycling(
+                "04 Nov 2022",
+                45,
+                25.0));
+
+        activities.Add(
+            new Swimming(
+                "05 Nov 2022",
+                40,
+                30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
